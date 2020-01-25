584a7c1b46c1b8be960fa8c64bb8adb23bfe55c9
Saim <saimh355@gmail.com>
2020-01-25 15:49:25 +0500

    Weather Application

--- app/src/main/java/com/example/weatherapplication/Common/Common.java
+++ app/src/main/java/com/example/weatherapplication/Common/Common.java
@@ -1,5 +1,8 @@
 package com.example.weatherapplication.Common;
 
+import android.location.Location;
+
 public class Common {
     public static final String APP_ID = "8b7fc1e484d451ba87524f9b5b537c85";
+    public static Location current_location;
 }
