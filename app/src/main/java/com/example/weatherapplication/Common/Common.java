0b3f05c68c428dacb836357c9a913c297a5a9289
Saim <saimh355@gmail.com>
2020-01-27 16:25:41 +0500

    Weather Application

--- app/src/main/java/com/example/weatherapplication/Common/Common.java
+++ app/src/main/java/com/example/weatherapplication/Common/Common.java
@@ -2,9 +2,25 @@ package com.example.weatherapplication.Common;
 
 import android.location.Location;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Common {
     public static final String APP_ID = "8b7fc1e484d451ba87524f9b5b537c85";
     public static Location current_location = null;
 
 
+    public static String convertUnitToDate(long dt) {
+        Date date = new Date(dt*1000L);
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm EEE MM yyyy");
+        String formatted = sdf.format(date);
+        return formatted;
+    }
+
+    public static String convertUnitToHour(long dt) {
+        Date date = new Date(dt*1000L);
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        String formatted = sdf.format(date);
+        return formatted;
+    }
 }
--- app/src/main/java/com/example/weatherapplication/TodayWeatherFragment.java
+++ app/src/main/java/com/example/weatherapplication/TodayWeatherFragment.java
@@ -12,11 +12,19 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.weatherapplication.Common.Common;
+import com.example.weatherapplication.Model.WeatherResult;
 import com.example.weatherapplication.Retrofit.IOpenWeatherMap;
 import com.example.weatherapplication.Retrofit.RetrofitClient;
+import com.google.android.material.snackbar.Snackbar;
+import com.squareup.picasso.Picasso;
 
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 import retrofit2.Retrofit;
 
 
@@ -53,7 +61,69 @@ public class TodayWeatherFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_today_weather, container, false);
+        View itemView =  inflater.inflate(R.layout.fragment_today_weather, container, false);
+        img_weather = (ImageView)itemView.findViewById(R.id.img_weather);
+        txt_city_name = (TextView)itemView.findViewById(R.id.txt_city_name);
+        txt_date_time = (TextView)itemView.findViewById(R.id.txt_date_time);
+        txt_description = (TextView)itemView.findViewById(R.id.txt_description);
+        txt_geo_coordinates = (TextView)itemView.findViewById(R.id.txt_geo_coordinates);
+        txt_humidity = (TextView)itemView.findViewById(R.id.txt_humadity);
+        txt_pressure = (TextView)itemView.findViewById(R.id.txt_pressure);
+        txt_sunrise = (TextView)itemView.findViewById(R.id.txt_sunrise);
+        txt_sunset = (TextView)itemView.findViewById(R.id.txt_sunset);
+        txt_temprature = (TextView)itemView.findViewById(R.id.txt_temprature);
+        txt_wind = (TextView)itemView.findViewById(R.id.txt_wind);
+        weather_panel = (LinearLayout)itemView.findViewById(R.id.weather_panel);
+
+        loading = (ProgressBar)itemView.findViewById(R.id.loading);
+
+        getweatherInformation();
+
+        return itemView;
+    }
+
+    private void getweatherInformation() {
+        compositeDisposable.add(mService.getWeatherByLatLng(String.valueOf(Common.current_location.getLatitude()),
+                String.valueOf(Common.current_location.getLongitude()),
+                Common.APP_ID,
+                "metric")
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(new Consumer<WeatherResult>() {
+            @Override
+            public void accept(WeatherResult weatherResult) throws Exception {
+                 //Load Image
+                Picasso.get().load(new StringBuilder("https://openweathermap.org/img/wn/")
+                .append(weatherResult.getWeather().get(0)
+                .getIcon())
+                .append(".png").toString()).into(img_weather);
+
+                //Load Information
+
+                txt_city_name.setText(weatherResult.getName());
+                txt_description.setText(new StringBuilder("Weather in ")
+                .append(weatherResult.getName()).toString());
+                txt_temprature.setText(new StringBuilder(String.valueOf(weatherResult.getMain().getTemp()))
+                .append("°C").toString());
+                txt_date_time.setText(Common.convertUnitToDate(weatherResult.getDt()));
+                txt_pressure.setText(new StringBuilder(String.valueOf(weatherResult.getMain().getPressure())).append(" hpa").toString());
+                txt_humidity.setText(new StringBuilder(String.valueOf(weatherResult.getMain().getHumidity()))
+                .append(" %").toString());
+                txt_sunrise.setText(Common.convertUnitToHour(weatherResult.getSys().getSunrise()));
+                txt_sunset.setText(Common.convertUnitToHour(weatherResult.getSys().getSunset()));
+                txt_geo_coordinates.setText(new StringBuilder("[")
+                .append(weatherResult.getCoord().toString()).append("]").toString());
+
+                //Display Panel
+                weather_panel.setVisibility(View.VISIBLE);
+                loading.setVisibility(View.GONE);
+            }
+        }, new Consumer<Throwable>() {
+            @Override
+            public void accept(Throwable throwable) throws Exception {
+                Toast.makeText(getActivity(), ""+throwable.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }));
     }
 
 }
