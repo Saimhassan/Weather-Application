65c1fe7e108e99748ac9d1673c06cc976d82ce0d
Saim <saimh355@gmail.com>
2020-01-27 15:21:04 +0500

    Weather Application

--- app/src/main/java/com/example/weatherapplication/TodayWeatherFragment.java
+++ app/src/main/java/com/example/weatherapplication/TodayWeatherFragment.java
@@ -8,12 +8,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.weatherapplication.Retrofit.IOpenWeatherMap;
+import com.example.weatherapplication.Retrofit.RetrofitClient;
+
+import io.reactivex.disposables.CompositeDisposable;
+import retrofit2.Retrofit;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TodayWeatherFragment extends Fragment {
+
+    ImageView img_weather;
+    TextView txt_city_name,txt_humidity,txt_sunrise,txt_sunset,txt_pressure,txt_temprature,txt_description,txt_date_time,txt_wind,txt_geo_coordinates;
+    LinearLayout weather_panel;
+    ProgressBar loading;
+
+    CompositeDisposable compositeDisposable;
+    IOpenWeatherMap mService;
 
     static TodayWeatherFragment instance;
 
@@ -22,6 +40,13 @@ public class TodayWeatherFragment extends Fragment {
         if (instance == null)
             instance = new TodayWeatherFragment();
         return instance;
+    }
+
+    public TodayWeatherFragment(){
+        compositeDisposable = new CompositeDisposable();
+        Retrofit retrofit = RetrofitClient.getInstance();
+        mService = retrofit.create(IOpenWeatherMap.class);
+
     }
 
     @Override
