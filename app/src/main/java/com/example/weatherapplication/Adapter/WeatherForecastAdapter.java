90b9e07a6550cf175b4c0342e25c7465e81c9c10
Saim <saimh355@gmail.com>
2020-01-28 15:40:30 +0500

    Weather Application

--- app/src/main/java/com/example/weatherapplication/Adapter/WeatherForecastAdapter.java
+++ app/src/main/java/com/example/weatherapplication/Adapter/WeatherForecastAdapter.java
@@ -1,4 +1,62 @@
 package com.example.weatherapplication.Adapter;
 
-public class WeatherForecastAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.weatherapplication.Model.WeatherForecastResult;
+import com.example.weatherapplication.R;
+import com.squareup.picasso.Picasso;
+
+public class WeatherForecastAdapter extends RecyclerView.Adapter<WeatherForecastAdapter.MyViewHolder> {
+
+    Context context;
+    WeatherForecastResult weatherForecastResult;
+
+    public WeatherForecastAdapter(Context context, WeatherForecastResult weatherForecastResult) {
+        this.context = context;
+        this.weatherForecastResult = weatherForecastResult;
+    }
+
+
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(context).inflate(R.layout.item_weather_forecast,parent,false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+         //Load Icon
+        Picasso.get().load(new StringBuilder("https://openweathermap.org/img/wn/")
+                .append(weatherForecastResult.list.get(position).weather.get(0).getIcon())
+                .append(".png").toString()).into(holder.img_weather);
+    }
+
+    @Override
+    public int getItemCount() {
+        return weatherForecastResult.list.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView txt_date_time,txt_description,txt_temperature;
+        ImageView img_weather;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            img_weather = (ImageView)itemView.findViewById(R.id.img_weather);
+            txt_date_time = (TextView)itemView.findViewById(R.id.txt_date);
+            txt_description = (TextView)itemView.findViewById(R.id.txt_description);
+            txt_temperature = (TextView)itemView.findViewById(R.id.txt_temprature);
+
+
+        }
+    }
 }
